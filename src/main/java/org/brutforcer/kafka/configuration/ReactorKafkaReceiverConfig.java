616b04a868f28adf754e790b680d518d5ee240ab
Titov Dmitriy <titov.dmitrii@otr.ru>
2022-05-21 11:25:39 +0300

    create reactive receivers config

---
+++ src/main/java/org/brutforcer/kafka/configuration/ReactorKafkaReceiverConfig.java
@@ -0,0 +1,49 @@
+package org.brutforcer.kafka.configuration;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.LongDeserializer;
+import org.brutforcer.kafka.events.KafkaEvent;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
+import reactor.kafka.receiver.KafkaReceiver;
+import reactor.kafka.receiver.ReceiverOptions;
+import reactor.kafka.receiver.internals.ConsumerFactory;
+import reactor.kafka.receiver.internals.DefaultKafkaReceiver;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+@ConditionalOnProperty(prefix = "kafka", name = "mode", havingValue = "reactive")
+public class ReactorKafkaReceiverConfig {
+
+    @Value("${kafka.host}")
+    private String host;
+    @Value("${kafka.group.id}")
+    private String groupId;
+    @Value("${kafka.topic.name}")
+    private String topicName;
+
+    @Bean
+    public KafkaReceiver<Long, KafkaEvent> kafkaReceiver() {
+
+        Map<String, Object> props = new HashMap<>();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, host);
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, LongDeserializer.class.getName());
+        props.put(JsonDeserializer.VALUE_DEFAULT_TYPE, "org.brutforcer.kafka.events.KafkaEvent");
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, "org.brutforcer.kafka.deserialize.EventDeserializer");
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, true);
+
+        return new DefaultKafkaReceiver<Long, KafkaEvent>(
+                ConsumerFactory.INSTANCE,
+                ReceiverOptions.<Long, KafkaEvent>create(props)
+                        .subscription(Collections.singleton(topicName))
+        );
+    }
+}
